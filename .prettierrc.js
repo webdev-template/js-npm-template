6cb8244c2dffbd19e92b27bc203fdb47f40280c4
Rabbitzzc <zzclovelcs@gmail.com>
2020-09-06 23:42:14 +0800

    feat: basic

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    printWidth: 120,
+    tabWidth: 4,
+    useTabs: false,
+    singleQuote: true,
+    semi: true,
+    TrailingCooma: "none",
+    bracketSpacing: true,
+    arrowParens: 'always',
+    mlWhitespaceSensitivity: 'ignore'
+}
