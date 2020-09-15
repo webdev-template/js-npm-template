f50ec650e7c201a24a4f7d78a775b5d81da10e85
Rabbitzzc <zzclovelcs@gmail.com>
2020-09-15 20:50:17 +0800

    chore: travis

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,32 @@
+import babel from "rollup-plugin-babel"
+
+// 压缩
+import {
+    terser
+} from "rollup-plugin-terser"
+
+// 输出文件夹清除
+import clear from "rollup-plugin-clear"
 const path = require('path')
 
 const resolve = function (filePath) {
-    return path.join(__dirname, '..', filePath)
+    console.log(path.join(__dirname,  filePath))
+    return path.join(__dirname,  filePath)
 }
 export default {
-    input: resolve('src/main.js'), // 入口文件
+    input: resolve('/src/index.js'), // 入口文件
     output: [{ // 出口文件
-        file: resolve('lib/index.js'),
+        file: resolve('dist/index.min.js'),
         format: 'umd',
         name: 'npm-lib',
-        plugins:[babel()]
-    }, {
-        file: resolve('lib/index.min.js'),
-        name: 'npm-lib',
-        plugins: [terser(),babel()]
+        plugins: [
+            clear({
+                targets: ["dist"]
+            }),
+            babel({
+                exclude: 'node_modules/**'
+            }), terser()
+        ]
     }],
     external: []
 }
